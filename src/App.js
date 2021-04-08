2d05380b88d5de79ec1a70479461deb2ba2e1696
ciiri <singleparty2012@gmail.com>
2021-04-08 17:03:57 +0800

    修改并push

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          修改后push到master分支触发cd
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
